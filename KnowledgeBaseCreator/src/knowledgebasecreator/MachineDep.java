0aa7f7c816c7ec1aa114f4eb63a061872907ce94
abhayprakash <abhay.prakash1@gmail.com>
2014-03-26 21:54:52 +0530

    trying to create neo4j graph

---
+++ KnowledgeBaseCreator/src/knowledgebasecreator/MachineDep.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package knowledgebasecreator;
+
+/**
+ *
+ * @author Abhay
+ */
+public class MachineDep {
+    public static String inputFilePath = "E:\\Projects\\NewsData\\KnowledgeBase\\headlines.txt";
+    public static String resultFilePath = "E:\\Projects\\NewsData\\KnowledgeBase\\entities.txt";
+}
